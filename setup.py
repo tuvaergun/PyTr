4ed0eb6b8c597efb94ae4cfc303f9997b61c50de
halitalptekin <diqit4ll@gmail.com>
2012-09-04 16:27:28 +0300

    Setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pytr',
+    version='0.1',
+    packages=['src', 'src.blog'],
+    url='http://www.pytr.org',
+    license='GPL',
+    author='Halit Alptekin',
+    author_email='info@halitalptekin.com',
+    description='Another Django Project'
+)
